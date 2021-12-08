f55bf9d63b8bb951fd1f3518332ae4f619224a5b
krishna-is-present <93341595+krishna-is-present@users.noreply.github.com>
2021-12-07 23:45:27 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  location = "eastus"
+}
+
+resource "aws_instance" "myInstance" {
+  ami           = "ami-06ce3edf0cff21f07"
+  instance_type = "t2.micro"
+  user_data     = <<-EOF
+                  #!/bin/bash
+                  sudo su
+                  yum -y install httpd
+                  echo "<p> My Instance! </p>" >> /var/www/html/index.html
+                  sudo systemctl enable httpd
+                  sudo systemctl start httpd
+                  EOF
+}
